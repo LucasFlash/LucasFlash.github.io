b75c80a4b63c4e66bdfa09d77c2d4371ccbe88cc
LucasFlash <89819357+LucasFlash@users.noreply.github.com>
2024-05-11 18:10:58 -0300

    Add files via upload

--- infor1.js
+++ infor1.js
@@ -1,7 +1,9 @@
 
-let y=0; 
+let y=0; let dd=[];
 let person = prompt("Please enter your name");
-let b = person;
+let b = parseInt(person);
+for (let i = 0; i < b; i++) {dd[i]= prompt("Please enter your name");}
+ let c = 0; let d=0; dd0= [0, 0, 0, 0, 0];
 
 let pp1=[
    ["¿En que año fue la Revolucion de Mayo?", "1810", "1916", "1853", 1],
@@ -18,7 +20,12 @@ let pp1=[
    ["Cual de estas batallas ganó San Martin?", "Caseros", "San Lorenzo", "Malvinas", 2],
    ["Cual de estos no fue presidente?", "Bilardo", "Menem", "Peron", 1],
    ["¿En que año fue la Independencia Argentina?", "1810", "1820", "1816", 3],
-   ["Cual de estos si fue presidente?", "Lavagna", "Frondizzi", "Belgrano", 2]
+   ["Cual de estos si fue presidente?", "Lavagna", "Frondizzi", "Belgrano", 2],
+   ["¿En que año fue la Revolucion de Mayo?", "1810", "1916", "1853", 1],
+   ["Cual de estos no fue presidente?", "Bilardo", "Menem", "Peron", 1],
+   ["¿En que año fue la Independencia Argentina?", "1810", "1820", "1816", 3],
+   ["Cual de estos si fue presidente?", "Lavagna", "Frondizzi", "Belgrano", 2],
+   ["¿Quien cruzó los Andes?", "Guemes", "Belgrano", "San Martin", 3],
 ];
 
 let pp2=[
@@ -26,7 +33,7 @@ let pp2=[
     ["Cual de estos es un Parque Nacional argentino?", "Los Glaciares", "Niagara", "San Pablo", 1],
     ["¿Cual de estos elementos abunda en Argentina?", "Diamantes", "Litio", "Tulipan", 2],
     ["Cuantos premios Nobel tiene Argentina?", "2", "5", "7", 2],
-    ["¿Cual es alimentos produce Argentina en grandes cantidades?", "Soja", "Tulipanes", "Palta", 1],
+    ["¿Cual de estos alimentos produce Argentina en grandes cantidades?", "Soja", "Tulipanes", "Palta", 1],
     ["¿Cual de estos animales no hay en Argentina", "1810", "1916", "1853", 1],
     ["Cual de estos es un Parque Nacional argentino?", "Bilardo", "Menem", "Peron", 1],
     ["¿Cual de estos elementos abunda en Argentina?", "1810", "1820", "1816", 3],
@@ -37,25 +44,37 @@ let pp2=[
     ["¿Cual de estos elementos abunda en Argentina?", "1810", "1820", "1816", 3],
     ["Cual de estos no es un volcan Argentina?", "Lavagna", "Frondizzi", "Belgrano", 2],
     ["¿Cual es alimentos no produce Argentina?", "Guemes", "Belgrano", "San Martin", 3],
+    ["¿Cual de estos animales no hay en Argentina", "Leones", "Pinguinos", "Carpinchos", 1],
+    ["Cual de estos es un Parque Nacional argentino?", "Los Glaciares", "Niagara", "San Pablo", 1],
+    ["¿Cual de estos elementos abunda en Argentina?", "Diamantes", "Litio", "Tulipan", 2],
+    ["Cuantos premios Nobel tiene Argentina?", "2", "5", "7", 2],
+    ["¿Cual de estos alimentos produce Argentina en grandes cantidades?", "Soja", "Tulipanes", "Palta", 1],
  ];
 
 let boton1 = document.querySelector('#boton1')
 boton1.addEventListener('click', () => { 
     let zz = document.getElementById("zz"); 
     let zzx = document.getElementById("zzx"); 
-    zz.innerHTML= `<div align="center"> Pregunta: <br>` + pp1[y][0] + `</div>`;
-    zzx.innerHTML= `<div align="center"> Opciones: <br>` + pp1[y][1] +  ` // ` + pp1[y][2] + ` // ` + pp1[y][3] + `</div>`;
-      y = y + 1;   b = b + 1; console.log(b);
+
+      if(y==0){d=0}; if(y==1){d=1};if(y==2){d=2};if(y==3){d=3};
+      if (y>=b & y<2*b) {d=y-b}; if (y>=2*b & y<3*b) {d=y-2*b}; if (y>=3*b & y<4*b) {d=y-3*b};
+       if (y>=4*b & y<5*b) {d=y-4*b}; if (y>=5*b & y<6*b) {d=y-5*b};  if (y>=6*b & y<7*b) {d=y-6*b};
+      if (y>=7*b & y<8*b) {d=y-7*b}; console.log("pregunta= " + y + "jugador " + d);y = y + 1;console.log(dd0);
+      zz.innerHTML= `<div align="center"><p style="font-size: 20px;"> <b>`+dd[d]+`</b> <br>` + pp1[y][0] + `</p></div>`;console.log(d);
+      zzx.innerHTML= `<div align="center"> <p style="font-size: 22px;"><br>A)<b>` + pp1[y][1] +  `</b> // B)<b>` + pp1[y][2] + `</b> // C)<b>` + pp1[y][3] + `</b></p></div>`;
 });
 
 let boton2 = document.querySelector('#boton2')
 boton2.addEventListener('click', () => { 
     let zz = document.getElementById("zz"); 
     let zzx = document.getElementById("zzx"); 
-    zz.innerHTML= `<div align="center"> Pregunta: <br>` + pp2[y][0] + `</div>`;
-    zzx.innerHTML= `<div align="center"> Opciones: <br>` + pp2[y][1] +
-     ` // ` + pp2[y][2] + ` // ` + pp2[y][3] + `</div>`;
-      y = y + 1;   
+
+    if(y==0){d=0}; if(y==1){d=1};if(y==2){d=2};if(y==3){d=3};
+    if (y>=b & y<2*b) {d=y-b}; if (y>=2*b & y<3*b) {d=y-2*b}; if (y>=3*b & y<4*b) {d=y-3*b};
+    if (y>=4*b & y<5*b) {d=y-4*b}; if (y>=5*b & y<6*b) {d=y-5*b};  if (y>=6*b & y<7*b) {d=y-6*b};
+   if (y>=7*b & y<8*b) {d=y-7*b}; console.log("pregunta= " + y + "jugador " + d);y = y + 1;console.log(dd0);
+   zz.innerHTML= `<div align="center"> <p style="font-size: 20px;"> <b>`+dd[d]+`</b> <br>` + pp2[y][0] + `</p></div>`;console.log(d);
+   zzx.innerHTML= `<div align="center"> <p style="font-size: 22px;"><br>A)<b>` + pp2[y][1] +  `</b> // B)<b>` + pp2[y][2] + `</b> // C)<b>` + pp2[y][3] + `</b></p></div>`;
 });
 
 
@@ -69,14 +88,15 @@ ba.addEventListener('click', () => {
         title: "INCORRECTO BOLUDO",
         text: '',
         icon: 'error',
-        confirmButtonText: 'OK' })} 
+        confirmButtonText: 'OK' }); dd0[d] = dd0[d] -50;} 
         else{zzq.innerHTML= y + ` Correcto `;
         Swal.fire({
             title: "CORRECTO GENIO",
             text: '',
             icon: 'success',
             confirmButtonText: 'OK' 
-        })}   
+        }); dd0[d] = dd0[d] + 100;
+    }   
 }});
 
 let bb = document.querySelector('#bb')
@@ -89,14 +109,14 @@ bb.addEventListener('click', () => {
         title: "INCORRECTO BOLUDO",
         text: '',
         icon: 'error',
-        confirmButtonText: 'OK' })} 
+        confirmButtonText: 'OK' }); dd0[d] = dd0[d] -50;} 
         else{zzq.innerHTML= y + ` Correcto `;
         Swal.fire({
             title: "CORRECTO GENIO",
             text: '',
             icon: 'success',
             confirmButtonText: 'OK' 
-        })}   
+        }); dd0[d] = dd0[d] + 100;}   
 }});
 
 let bc = document.querySelector('#bc')
@@ -109,12 +129,12 @@ bc.addEventListener('click', () => {
         title: "INCORRECTO BOLUDO",
         text: '',
         icon: 'error',
-        confirmButtonText: 'OK' })} 
+        confirmButtonText: 'OK' }); dd0[d] = dd0[d] -50;} 
         else{zzq.innerHTML= y + ` Correcto `;
         Swal.fire({
             title: "CORRECTO GENIO",
             text: '',
             icon: 'success',
             confirmButtonText: 'OK' 
-        })}   
+        }); dd0[d] = dd0[d] + 100;}   
 }});
